8777bcf5bc18b67f5214062e12dcd6616e05aef0
Ferhat Abdalimov <abdalimov111@gmail.com>
2022-04-10 16:49:18 +0300

    changes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./style.scss";
+function App() {
+  return (
+    <div className="app">
+      <div className="top">
+        <img src="assets/blob4.png" alt="blob" />
+      </div>
+      <div className="content">
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button>Start quiz</button>
+      </div>
+      <div className="bottom">
+        <img src="assets/blob5.png" alt="blob" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
